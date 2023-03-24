32ac32621656f57b4b630ba241374f4dbc1e1fb9
RosaMonro <rosamaria.monro@gmail.com>
2023-03-24 21:50:49 +0100

    cambios en grids

--- template-app.php
+++ template-app.php
@@ -7,7 +7,7 @@
     <div class="product__title">
             <h1 class="margin-bottom-0"><?php the_title(); ?></h1>
             <a href="https://play.google.com/store/apps/details?id=com.MonteroGames.ColorinesNeuroGames" target="_blank">
-                <img class="button--google" src="/images/Botones/google.png" alt="Botón con enlace a la playstore">
+                <img class="button--google" src="<?php bloginfo ( 'template_url' ); ?>/images/Botones/google.png" alt="Botón con enlace a la playstore">
             </a>
     </div>
 
